ed23c559ee13158ce900a710a5c69ecaa31fb96f
Miquéias Nascimento <nmiqueias25@gmail.com>
2024-11-14 15:59:01 -0300

    Update dialogflowFunction.js

--- functions/dialogflowFunction.js
+++ functions/dialogflowFunction.js
@@ -2,15 +2,14 @@ const functions = require("firebase-functions");
 const dialogflow = require("dialogflow");
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(async (request, response) => {
-  // Crie um novo cliente Dialogflow
+  
   const sessionClient = new dialogflow.SessionsClient();
-  // ID do projeto no Dialogflow
+ 
   const sessionPath = sessionClient.projectAgentSessionPath("chatbot-ia-49080", "uma-sessao-unica"); 
 
-  // Obtenha o texto da solicitação
   const requestText = request.body.queryResult.queryText;
 
-  // Crie uma nova solicitação de detecção de intenção
+ 
   const requestDialogflow = {
     session: sessionPath,
     queryInput: {
@@ -21,12 +20,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(async (request
     },
   };
 
-  // Envie a solicitação para o Dialogflow
+  
   const responses = await sessionClient.detectIntent(requestDialogflow);
   const result = responses[0].queryResult;
 
-  // Envie a resposta do Dialogflow para o usuário
+  
   response.send({
     fulfillmentText: result.fulfillmentText,
   });
-});
+})
